0a6673203840dd123096fc4883b89f1b9746ecdf
Max Cantor <max@maxcantor.net>
2015-12-08 22:35:49 -0500

    Solution for form tag validation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/authors/new", to: "authors#new", as: "new_author"
+  post "/authors", to: "authors#create"
+  get "/authors/:id", to: "authors#show", as: "author"
+  get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
+  patch "/authors/:id", to: "authors#update"
+
+  get "/posts/new", to: "posts#new", as: "new_post"
+  post "/posts", to: "posts#create"
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "/posts/:id", to: "posts#update"
 end
