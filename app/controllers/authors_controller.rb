690ded166e8927c5db8ac94c4bf150ddd93c1635
Charles Pletcher <cpletcher2@gmail.com>
2016-03-01 11:45:26 -0500

    Make controllers even more bare bones
    Fixes #2
    
    And gives students more to do (rhyme unintentional)
    
    Add license and contributing files

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,13 +8,9 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.new(author_params)
+    @author = Author.create(author_params)
 
-    if @author.save
-      redirect_to author_path(@author)
-    else
-      render :new
-    end
+    redirect_to author_path(@author)
   end
 
   private
