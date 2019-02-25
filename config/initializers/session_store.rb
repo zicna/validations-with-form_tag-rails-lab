24385cc30e9115ed93f9391583f4a3beaf5873d9
maxwellbenton <maxwellbenton@gmail.com>
2019-02-25 14:59:37 -0500

    rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_validations-with-form_tag-rails-lab_session'
+Rails.application.config.session_store :cookie_store, key: '_validations_with_form_tag_rails_lab_session'
