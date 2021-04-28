8f18b96d7973209c024667fe3e19616103666c25
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-04-28 00:34:15 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,13 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
 
-    redirect_to author_path(@author)
   end
 
   private
